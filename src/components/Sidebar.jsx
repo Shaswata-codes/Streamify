b744a3ac74e8cd4c800e7147983c4ef08629e363
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2026-04-28 20:45:46 +0530

    Sidebar done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,44 @@ const SideBar = () => {
         <div className='w-[25%] h-full p-2 text-white flex flex-col gap-2'>
             <div className='bg-[#121212] rounded flex flex-col justify-around p-4'>
                 <div className='flex items-center gap-3 cursor-pointer'>
-                    <img src={assets.home_icon} alt="" className='w-6' />
+                    <img className='w-6' src={assets.home_icon} alt="" />
                     <p className='font-bold'>Home</p>
+                </div>
+            </div>
+
+            <div className='bg-[#121212] rounded flex flex-col justify-around p-4'>
+                <div className='flex items-center gap-3 cursor-pointer'>
+                    <img className='w-6' src={assets.home_icon} alt="" />
+                    <p className='font-bold'>Search</p>
+                </div>
+            </div>
+
+            <div className='bg-[#121212] h-[85%] rounded'>
+                <div className='flex items-center justify-between p-4'>
+                    <div className='flex items-center gap-3'>
+                        <img className='w-8' src={assets.stack_icon} alt="" />
+                        <p className='font-semibold'>Your Library</p>
+                    </div>
+
+                    <div className='flex items-center gap-3'>
+                        <img className='w-5 cursor-pointer' src={assets.arrow_icon} alt="" />
+                        <img className='w-5 cursor-pointer' src={assets.plus_icon} alt="" />
+                    </div>
+                </div>
+
+                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                    <h1>Create a playlist</h1>
+                    <p className='font-light'>It's easy, we will help you through</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>
+                        Create Play
+                    </button>
+                </div>
+                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
+                    <h1>Podcasts to follow</h1>
+                    <p className='font-light'>We will keep you updated on new episodes</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>
+                        Browse Podcasts
+                    </button>
                 </div>
             </div>
         </div>
